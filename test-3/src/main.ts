9b5f3d8a70e159a596448b272fe966ece76b2c06
BeycanDeveloper <halilbeycan0@gmail.com>
2024-11-07 21:59:50 +0700

    .

---
+++ test-3/src/main.ts
@@ -0,0 +1,51 @@
+import { createAppKit } from "@reown/appkit";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { mainnet, arbitrum } from "@reown/appkit/networks";
+import { SolanaAdapter } from "@reown/appkit-adapter-solana";
+import { solana, solanaDevnet } from "@reown/appkit/networks";
+const projectId = "113d9f5689edd84ff230c2a6d679c80c";
+
+document
+  .getElementById("create-evm")
+  ?.addEventListener("click", async function () {
+    const modal = createAppKit({
+      adapters: [new EthersAdapter()],
+      networks: [mainnet, arbitrum],
+      projectId,
+    });
+
+    modal.open();
+  });
+
+document
+  .getElementById("create-solana")
+  ?.addEventListener("click", async function () {
+    const modal = createAppKit({
+      adapters: [
+        new SolanaAdapter({
+          wallets: [],
+        }),
+      ],
+      networks: [solana, solanaDevnet],
+      projectId,
+    });
+
+    modal.open();
+  });
+
+document
+  .getElementById("create-multichain")
+  ?.addEventListener("click", async function () {
+    const modal = createAppKit({
+      adapters: [
+        new EthersAdapter(),
+        new SolanaAdapter({
+          wallets: [],
+        }),
+      ],
+      networks: [mainnet, arbitrum, solana, solanaDevnet],
+      projectId,
+    });
+
+    modal.open();
+  });
